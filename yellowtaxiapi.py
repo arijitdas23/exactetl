3baad8c83a1534bdda932093671de118ff2dc78e
arijitdas23 <arijitdas23@gmail.com>
2021-01-07 23:27:33 +0530

    updated

--- yellowtaxiapi.py
+++ yellowtaxiapi.py
@@ -4,16 +4,17 @@ import urllib
 import pandas as pd
 
 def opendbconnection():
+    """
     sql_server_name = os.environ.get('SQL_SERVER_NAME')
     sql_db_name = os.environ.get('SQL_DB_NAME')
     db_user_name = os.environ.get('SQL_USER_NAME')
     db_user_pswd = os.environ.get('SQL_PSWD')
-    """
+
     sql_server_name = 'azsqldbserver1988.database.windows.net'
     sql_db_name = 'azsqldb1988'
     db_user_name = 'demouser'
     db_user_pswd = 'Iamsmart@2020'
-    """
+
 
     sql_connection_settings = f'SERVER={sql_server_name};PORT=1433;DATABASE={sql_db_name};UID={db_user_name};PWD={db_user_pswd}'
     sql_connection_string = f'DRIVER={{FreeTDS}};{sql_connection_settings};TDS_VERSION=8.0;'
@@ -35,7 +36,7 @@ def opendbconnection():
                                       conStrDct.get('server'), \
                                       conStrDct.get("database"), \
                                       conStrDct.get("driver")), echo=False)
-    """
+
     return engine
 
 def closedbconnection(conn):
@@ -62,7 +63,7 @@ def readcustomer():
     except Exception as e:
         print(str(e))
         success = False
-
+    closedbconnection(conn)
     return df, success
 
 def getcustomernameapi():
