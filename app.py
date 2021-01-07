a5387a3d0140ed738ec97b3efb8da39a20c9f21b
arijitdas23 <arijitdas23@gmail.com>
2021-01-07 20:48:57 +0530

    updated

--- app.py
+++ app.py
@@ -1,11 +1,13 @@
 import flask
+import os
 from flask import Flask,request,abort
 
 app = Flask(__name__)
 
 @app.route('/api/', methods=['GET'])
 def rootapi():
-    return "Hello World"
+    db_user_nme = os.environ.get('DB_USER_NAME')
+    return "Hello World"+db_user_nme
 
 if __name__=='__main__':
     app.run(debug=True,host='0.0.0.0')
