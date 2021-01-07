070915d887bc7a842337688b650a59736d594f3e
arijitdas23 <arijitdas23@gmail.com>
2021-01-07 22:37:30 +0530

    updated to db call

--- app.py
+++ app.py
@@ -1,13 +1,14 @@
 import flask
 import os
 from flask import Flask,request,abort
+from yellowtaxiapi import *
 
 app = Flask(__name__)
 
 @app.route('/api/', methods=['GET'])
 def rootapi():
-    db_user_nme = os.environ.get('SQL_USER_NAME')
-    return "Hello World"+db_user_nme
+    retstr = getcustomernameapi()
+    return "Hello World "+retstr[1:-1]
 
 if __name__=='__main__':
     app.run(debug=True,host='0.0.0.0')
---
+++ yellowtaxiapi.py
@@ -0,0 +1,76 @@
+import os
+import sqlalchemy as sa
+import urllib
+import pandas as pd
+
+def opendbconnection():
+    sql_server_name = os.environ.get('SQL_SERVER_NAME')
+    sql_db_name = os.environ.get('SQL_DB_NAME')
+    db_user_name = os.environ.get('SQL_USER_NAME')
+    db_user_pswd = os.environ.get('SQL_PSWD')
+    """
+    sql_server_name = 'azsqldbserver1988.database.windows.net'
+    sql_db_name = 'azsqldb1988'
+    db_user_name = 'demouser'
+    db_user_pswd = 'Iamsmart@2020'
+    """
+
+    sql_connection_settings = f'SERVER={sql_server_name};PORT=1433;DATABASE={sql_db_name};UID={db_user_name};PWD={db_user_pswd}'
+    sql_connection_string = f'DRIVER={{FreeTDS}};{sql_connection_settings};TDS_VERSION=8.0;'
+
+    params = urllib.parse.quote_plus(sql_connection_string)
+    engine = sa.create_engine("mssql+pyodbc:///?odbc_connect=%s" % params)
+    """
+    conStrDct = {'user': 'demouser',
+                 'password': 'Iamsmart@2020',
+                 'server': 'azsqldbserver1988.database.windows.net',
+                 'database': 'azsqldb1988',
+                 'driver': 'SQL+Server'
+                 # 'driver':'FreeTDS'
+                 }
+
+    engine = sa.create_engine("mssql+pyodbc://{}:{}@{}/{}?driver={}" \
+                              .format(conStrDct.get('user'), \
+                                      conStrDct.get("password"), \
+                                      conStrDct.get('server'), \
+                                      conStrDct.get("database"), \
+                                      conStrDct.get("driver")), echo=False)
+    """
+    return engine
+
+def closedbconnection(conn):
+    conn.dispose()
+
+def prepareproccall(procname,paramlist):
+    val = "?,"*len(paramlist)
+    val = val[:-1]
+    callproc = "set nocount on;{call "+procname+"("+val+")}"
+    return callproc
+
+def readcustomer():
+    conn = opendbconnection()
+    df = pd.DataFrame([],columns=['FirstName'])
+    procname = 'dbo.sp_readcustomer'
+    paramlist = [1]
+    callproc = prepareproccall(procname,paramlist)
+    try:
+        with conn.begin() as connection:
+            results = connection.execute(callproc,paramlist).fetchall()
+            if len(results)>0:
+                df = pd.DataFrame(results,columns=['FirstName'])
+            success = True
+    except Exception as e:
+        print(str(e))
+        success = False
+
+    return df, success
+
+def getcustomernameapi():
+    customerdf, success = readcustomer()
+    out = customerdf.to_json(orient='records')
+    return out
+
+out = getcustomernameapi()
+print(out[1:-1])
+
+
