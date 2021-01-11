5dbd4c60c2648d95d4ca34e567e9dbe05a1f6fb3
arijitdas23 <arijitdas23@gmail.com>
2021-01-11 17:41:28 +0530

    YellowTaxi api added

--- app.py
+++ app.py
@@ -5,11 +5,25 @@ from yellowtaxiapi import *
 
 app = Flask(__name__)
 
-@app.route('/api/', methods=['GET'])
-def rootapi():
+@app.route('/')
+@app.route('/hello')
+def helloworld():
+    return "HelloWorld"
+
+@app.route('/api/tip/<trip_year>/<max>', defaults={'trip_quarter': None}, methods=['GET'])
+@app.route('/api/tip/<trip_year>/<trip_quarter>/<max>', methods=['GET'])
+def maxtippctgapi(trip_year,trip_quarter,max):
     # comment
-    retstr = getcustomernameapi()
-    return "Hello World "+retstr
+    retstr = getMaxTipPercentage(trip_year, trip_quarter)
+    return retstr
+
+@app.route('/api/speed/<trip_year>/<max>', defaults={'trip_month': 1,'trip_day':1}, methods=['GET'])
+@app.route('/api/speed/<trip_year>/<trip_month>/<max>', defaults={'trip_day':1}, methods=['GET'])
+@app.route('/api/tip/<trip_year>/<trip_month>/<trip_day>/<max>', methods=['GET'])
+def maxtripspeedapi(trip_year,trip_month,trip_day,max):
+    # comment
+    retstr = getMaxTripSpeed(trip_year, trip_month, trip_day)
+    return retstr
 
 if __name__=='__main__':
-    app.run(debug=True,host='0.0.0.0')
+    app.run(debug=True,host='0.0.0.0',port=5000)
--- yellowtaxiapi.py
+++ yellowtaxiapi.py
@@ -2,6 +2,8 @@ import os
 import sqlalchemy as sa
 import urllib
 import pandas as pd
+import json
+
 
 def opendbconnection():
     """
@@ -10,6 +12,7 @@ def opendbconnection():
     db_user_name = os.environ.get('SQL_USER_NAME')
     db_user_pswd = os.environ.get('SQL_PSWD')
 
+    """
     """
     sql_server_name = 'azsqldbserver1988.database.windows.net'
     sql_db_name = 'azsqldb1988'
@@ -37,40 +40,102 @@ def opendbconnection():
                                       conStrDct.get('server'), \
                                       conStrDct.get("database"), \
                                       conStrDct.get("driver")), echo=False)
-    """
 
     return engine
+
 
 def closedbconnection(conn):
     conn.dispose()
 
-def prepareproccall(procname,paramlist):
-    val = "?,"*len(paramlist)
+
+def prepareproccall(procname, paramlist):
+    val = "?," * len(paramlist)
     val = val[:-1]
-    callproc = "set nocount on;{call "+procname+"("+val+")}"
+    callproc = "set nocount on;{call " + procname + "(" + val + ")}"
     return callproc
 
-def readcustomer():
+
+def readusingsqldbproc(procname, paramlist, columnlist):
     conn = opendbconnection()
-    df = pd.DataFrame([],columns=['FirstName'])
-    procname = 'dbo.sp_readcustomer'
-    paramlist = [2]
-    callproc = prepareproccall(procname,paramlist)
+    df = pd.DataFrame([], columns=columnlist)
+    callproc = prepareproccall(procname, paramlist)
     try:
         with conn.begin() as connection:
-            results = connection.execute(callproc,paramlist).fetchall()
-            if len(results)>0:
-                df = pd.DataFrame(results,columns=['FirstName'])
+            results = connection.execute(callproc, paramlist).fetchall()
+            if len(results) > 0:
+                df = pd.DataFrame(results, columns=columnlist)
             success = "Correct"
     except Exception as e:
         success = str(e)
     closedbconnection(conn)
     return df, success
 
+
+'''
 def getcustomernameapi():
     customerdf, success = readcustomer()
     out = customerdf.to_json(orient='records')
     return out+success
+'''
 
 
+def getMaxTipPercentage(trip_year, trip_quarter):
+    paramlist = []
+    if trip_year is not None:
+        paramlist.append(trip_year)
+    if trip_quarter is not None:
+        paramlist.append(trip_quarter)
+    procname = 'sp_YelloTaxiTipInsight'
+    columnlist = ['trip_year', 'trip_quarter', 'max_tip_pctg']
+    dftippercentage, success = readusingsqldbproc(procname, paramlist, columnlist)
+    ret_json_str = ''
+    if len(dftippercentage.head(1)) > 0:
+        dftippercentage['max_tip_pctg'] = \
+            dftippercentage['max_tip_pctg'].round().astype('int')
+        dftippercentage = dftippercentage.rename(
+            columns={'max_tip_pctg': 'maxTipPercentage', 'trip_quarter': 'quarter'})
+        if dftippercentage.shape[0] == 1:
+            dftippercentage = dftippercentage.drop(['trip_year', 'quarter'], axis=1)
+            ret_json_str = dftippercentage.to_json(orient='records')
+        else:
+            j = dftippercentage.groupby(['trip_year'], as_index=False) \
+                .apply(lambda x: x[['quarter', 'maxTipPercentage']].to_dict('r')) \
+                .reset_index().drop('index', axis=1) \
+                .rename(columns={0: 'maxTipPercentages'}) \
+                .to_json(orient='records')
+            ret_json_str = json.dumps(json.loads(j), indent=2, sort_keys=False)
 
+    else:
+        ret_json_str = "No data found for this criteria"
+
+    return ret_json_str[1:-1]
+
+
+def getMaxTripSpeed(trip_year, trip_month, trip_day):
+    paramlist = []
+    if trip_year != None:
+        paramlist.append(trip_year)
+    if trip_month != None:
+        paramlist.append(trip_month)
+    if trip_day != None:
+        paramlist.append(trip_day)
+    procname = 'sp_YelloTaxiTripSpeedInsight'
+    columnlist = ['trip_year', 'trip_hour_of_day', 'max_trip_speed']
+    dftripspeed, success = readusingsqldbproc(procname, paramlist, columnlist)
+    ret_json_str = ''
+    if len(dftripspeed.head(1)) > 0:
+        dftripspeed['max_trip_speed'] = \
+            dftripspeed['max_trip_speed'].round().astype('int')
+        dftripspeed = dftripspeed.rename(
+            columns={'max_trip_speed': 'maxSpeed', 'trip_hour_of_day': 'hour'})
+        dftripspeed['hour'] = dftripspeed['hour']+1
+        dftripspeed = dftripspeed.sort_values(by=['hour'])
+        j = dftripspeed.groupby(['trip_year'], as_index=False) \
+            .apply(lambda x: x[['hour', 'maxSpeed']].to_dict('r')) \
+            .reset_index().drop('index', axis=1) \
+            .rename(columns={0: 'tripSpeeds'}) \
+            .to_json(orient='records')
+        ret_json_str = json.dumps(json.loads(j), indent=2, sort_keys=False)
+    else:
+        ret_json_str = "No data found for this criteria"
+    return ret_json_str[1:-1]
