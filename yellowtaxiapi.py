8663c5fa0a549d41283e00975f08dcbfe131df11
arijitdas23 <arijitdas23@gmail.com>
2021-01-11 17:44:11 +0530

    updated

--- yellowtaxiapi.py
+++ yellowtaxiapi.py
@@ -6,19 +6,18 @@ import json
 
 
 def opendbconnection():
-    """
+
     sql_server_name = os.environ.get('SQL_SERVER_NAME')
     sql_db_name = os.environ.get('SQL_DB_NAME')
     db_user_name = os.environ.get('SQL_USER_NAME')
     db_user_pswd = os.environ.get('SQL_PSWD')
 
     """
-    """
     sql_server_name = 'azsqldbserver1988.database.windows.net'
     sql_db_name = 'azsqldb1988'
     db_user_name = 'demouser'
     db_user_pswd = 'Iamsmart@2020'
-
+    """
 
     sql_connection_settings = f'SERVER={sql_server_name};PORT=1433;DATABASE={sql_db_name};UID={db_user_name};PWD={db_user_pswd}'
     sql_connection_string = f'DRIVER={{FreeTDS}};{sql_connection_settings};TDS_VERSION=8.0;'
@@ -40,7 +39,7 @@ def opendbconnection():
                                       conStrDct.get('server'), \
                                       conStrDct.get("database"), \
                                       conStrDct.get("driver")), echo=False)
-
+    """
     return engine
 
 
