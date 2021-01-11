50f9009643c15e5d788ff7f55b227229d5cfaf0b
arijitdas23 <arijitdas23@gmail.com>
2021-01-11 18:06:10 +0530

    updated app.py

--- app.py
+++ app.py
@@ -26,4 +26,4 @@ def maxtripspeedapi(trip_year,trip_month,trip_day,max):
     return retstr
 
 if __name__=='__main__':
-    app.run(debug=True,host='0.0.0.0',port=5000)
+    app.run(debug=True,host='0.0.0.0')
